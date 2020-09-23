fb1b53061929d62a43e571f411faa08c3560f2a6
MallardDuck <self@danpock.me>
2020-09-23 17:34:05 -0400

    Add more tests for UnfilledIf test

--- tests/Rules/UnfilledIfTest.php
+++ tests/Rules/UnfilledIfTest.php
@@ -6,7 +6,7 @@ use MallardDuck\OpinionatedValidator\Tests\BaseTest;
 
 class UnfilledIfTest extends BaseTest
 {
-    public function testValidateUnfilledIf()
+    public function testValidateUnfilledIfShapeGeneratorExample()
     {
         $basicUnfilledRules = [
             'shape'  => 'required',
@@ -51,6 +51,57 @@ class UnfilledIfTest extends BaseTest
             'shape' => 'rect',
             'height' => '64',
             'width' => '96',
+        ], $basicUnfilledRules);
+        $this->assertTrue($v->passes());
+    }
+
+    public function testValidateUnfilledIfNameExample()
+    {
+        $basicUnfilledRules = [
+            'mode'       => 'required',
+            'full_name'  => 'required_if:mode,full|unfilled_if:mode,split',
+            'first_name' => 'unfilled_if:mode,full',
+            'last_name'  => 'unfilled_if:mode,full',
+        ];
+
+        $v = $this->getValidator()->make([
+            'mode' => 'none'
+        ], $basicUnfilledRules);
+        $this->assertTrue($v->passes());
+
+        $v = $this->getValidator()->make([
+            'mode'      => 'full',
+            'full_name' => 'Ricky Bobby',
+        ], $basicUnfilledRules);
+        $this->assertTrue($v->passes());
+
+        $v = $this->getValidator()->make([
+            'mode'       => 'full',
+            'full_name'  => 'Ricky Bobby',
+            'first_name' => 'Richard',
+        ], $basicUnfilledRules);
+        $this->assertTrue($v->fails());
+
+        $v = $this->getValidator()->make([
+            'mode'       => 'full',
+            'full_name'  => 'Ricky Bobby',
+            'first_name' => 'Richard',
+            'last_name'  => 'Robertson',
+        ], $basicUnfilledRules);
+        $this->assertTrue($v->fails());
+
+        $v = $this->getValidator()->make([
+            'mode'       => 'split',
+            'full_name'  => 'Ricky Bobby',
+            'first_name' => 'Richard',
+            'last_name'  => 'Robertson',
+        ], $basicUnfilledRules);
+        $this->assertTrue($v->fails());
+
+        $v = $this->getValidator()->make([
+            'mode'       => 'split',
+            'first_name' => 'Richard',
+            'last_name'  => 'Robertson',
         ], $basicUnfilledRules);
         $this->assertTrue($v->passes());
     }
