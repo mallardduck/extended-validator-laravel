0343c72d21f2ea3287683396979f5d807d0c3e70
MallardDuck <self@danpock.me>
2020-09-26 22:39:43 -0400

    refactor switch to ifs

--- src/ExtendedValidatorServiceProvider.php
+++ src/ExtendedValidatorServiceProvider.php
@@ -4,6 +4,7 @@ namespace MallardDuck\ExtendedValidator;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Factory;
+use MallardDuck\ExtendedValidator\Rules\BaseRule;
 
 class ExtendedValidatorServiceProvider extends ServiceProvider
 {
@@ -18,17 +19,16 @@ class ExtendedValidatorServiceProvider extends ServiceProvider
         $baseValidator = app('validator');
         foreach (RuleManager::allRules() as $ruleType => $rules) {
             foreach ($rules as $key => $rule) {
+                /** @var BaseRule $rule */
                 $rule = new $rule();
-                switch ($ruleType) {
-                    case "rules":
-                        $baseValidator->extend($rule->name, $rule->callback, $rule->message);
-                        break;
-                    case "implicit":
-                        $baseValidator->extendImplicit($rule->name, $rule->callback, $rule->message);
-                        break;
-                    case "dependent":
-                        $baseValidator->extendDependent($rule->name, $rule->callback, $rule->message);
-                        break;
+                if ('rules' === $ruleType) {
+                    $baseValidator->extend($rule->name, $rule->callback, $rule->message);
+                }
+                if ('implicit' === $ruleType) {
+                    $baseValidator->extendImplicit($rule->name, $rule->callback, $rule->message);
+                }
+                if ('dependent' === $ruleType) {
+                    $baseValidator->extendDependent($rule->name, $rule->callback, $rule->message);
                 }
                 if (is_callable($rule->replacer)) {
                     $baseValidator->replacer($rule->name, $rule->replacer);
