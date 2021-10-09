370f2c72d0526e6809a15f4e96bf45a2253fc5cd
Dan Pock <self@danpock.me>
2021-10-09 17:49:39 -0400

    update to new phpcs config

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,18 +1,20 @@
 <?php
 
-$finder = \Symfony\Component\Finder\Finder::create()
+$finder = PhpCsFixer\Finder::create()
     ->notPath('vendor')
     ->in(__DIR__)
-    ->name('*.php')
+    ->name("*.php")
     ->ignoreDotFiles(true)
-    ->ignoreVCS(true);
+    ->ignoreVCS(true)
+;
 
-
-return \PhpCsFixer\Config::create()
+return (new \PhpCsFixer\Config())
     ->setRules([
         '@PSR12' => true,
+        //'strict_param' => true,
         'array_syntax' => ['syntax' => 'short'],
         'ordered_imports' => ['sortAlgorithm' => 'alpha'],
         'no_unused_imports' => true,
+        'lineLimit' => 120,
     ])
     ->setFinder($finder);
