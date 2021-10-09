0d6f3648ac3467cb490a7bd78684e40cab7eefbf
Dan Pock <self@danpock.me>
2021-10-09 17:11:21 -0400

    remove unused imports

--- src/Rules/NotInIf.php
+++ src/Rules/NotInIf.php
@@ -4,10 +4,7 @@ declare(strict_types=1);
 
 namespace MallardDuck\ExtendedValidator\Rules;
 
-use Illuminate\Support\Str;
 use Illuminate\Validation\Validator;
-use InvalidArgumentException;
-use MallardDuck\ExtendedValidator\ValidatorProxy;
 
 final class NotInIf extends BaseRule
 {
